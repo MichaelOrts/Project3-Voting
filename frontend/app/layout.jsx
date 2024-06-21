6863076680bde7041a3c2a5d7217c53fe7a6ab17
David Fradel <david.fradel@gmail.com>
2024-06-21 13:32:10 +0200

    feat: update favicon

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -8,12 +8,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Crypto Consensus",
-  description: "A simple app to vote on the proposed changes to the web3 ecosystem.",
+  description: "A simple app to vote on the proposed changes to the web3 ecosystem."
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" sizes="any" type="image/ico" />
+      </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <CustomRainbowKitProvider>
           <div className="flex flex-col flex-grow min-h-screen">
