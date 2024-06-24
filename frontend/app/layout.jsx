3b606086505052c2b81a9f193802ad274e3cc472
Michael Orts <michael.orts.86@gmail.com>
2024-06-24 15:14:57 +0200

    add Toaster component

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
             <Footer />
           </div>
         </CustomRainbowKitProvider>
+        <Toaster/>
       </body>
     </html>
   );
